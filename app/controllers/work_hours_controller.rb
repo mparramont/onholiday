e874da5321488e3f50a94d3d68fab548c01e0b45
emmanuelmillionaer <emmanuelmillionaer@users.noreply.github.com>
2015-04-19 19:32:28 +0200

    refactor change json jquer

--- app/controllers/work_hours_controller.rb
+++ app/controllers/work_hours_controller.rb
@@ -1,12 +1,10 @@
 class WorkHoursController < ApplicationController
   before_action :set_fields, except: :index
+  before_action :define_timetable_user, only: :index
+  before_action :define_timetable_day, only: :index
 
   def index
-    @day = Date.today
-    if params[:date]
-      @day = Date.parse(params[:date])
-    end
-    @work_hours = current_user.work_hours.where(day: @day.beginning_of_week..@day.end_of_week)
+    @work_hours = @timetable_user.work_hours.where(day: @day.beginning_of_week..@day.end_of_week)
     @work_hours = @work_hours.to_json(only: [:hour, :day])
   end
 
@@ -27,11 +25,26 @@ class WorkHoursController < ApplicationController
     head :created
   end
 
-
+ private
   def set_fields
       @user = User.find(params[:user_id])
       @day = params[:day]
       @hour = params[:hour]
+  end
+
+  def define_timetable_user
+    if current_user.admin?
+      @timetable_user = User.find(params[:user_id]) if params[:user_id]
+      redirect_to admin_users_path if @timetable_user.nil?
+    end
+    @timetable_user = @timetable_user || current_user
+  end
+
+  def define_timetable_day
+    @day = Date.today
+    if params[:date]
+      @day = Date.parse(params[:date])
+    end
   end
 
 end
